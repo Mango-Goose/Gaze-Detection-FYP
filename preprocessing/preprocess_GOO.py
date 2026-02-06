b08f5afac40a6a9b739293ed9a92843b76f59f1e
Mango-Goose <113191075+Mango-Goose@users.noreply.github.com>
2026-02-06 16:30:30 +0000

    Completed Preprocessing script
    Successfully formats the dataset into what the model (GazeLLE) is expecting, may need to be edited further in the future to better fit requirements.

--- preprocessing/preprocess_GOO.py
+++ preprocessing/preprocess_GOO.py
@@ -19,7 +19,6 @@ args = parser.parse_args()
 def main(DATA_PATH):
     #TEST
 
-
     #check if dataset downloaded, if not stream it from huggingface
     if len(os.listdir(DATA_PATH)) == 0:
         dataset = load_dataset("markytools/goosyntheticv3", split="test", streaming=True)  #issue with this - creates an iterable dataset not a dataset - doesnt act the same.
@@ -29,8 +28,17 @@ def main(DATA_PATH):
         print("loading dataset from local path")
 
     #need to differentiate between person bounding boxes and product bounding boxes - label of 25
+    TEST_FRAMES = []
 
-    for i in range(1):
+    for i in range(dataset.num_rows):
+
+        #get file location
+        img = dataset['image'][i]
+        filename = img.filename
+        location = os.path.join(DATA_PATH, filename)
+        width, height = img.size
+
+        #get head bbox
         bboxes = re.findall(r"\[(.*?)\]", dataset['bboxes'][i][1:-1])
         labels = dataset['labels'][i][1:-1].split(", ") #getting both bboxes and labels as lists
 
@@ -40,6 +48,97 @@ def main(DATA_PATH):
         bboxes = bboxes.remove(bboxes[l_idx]) 
         labels = labels.remove('25')
         
+        #xmin, xmax, ymin, ymax etc
+        xmin, ymin, xmax, ymax = head_bbox[1:-1].split(", ")
+        xmin = float(xmin)
+        ymin = float(ymin)
+        xmax = float(xmax)
+        ymax = float(ymax)
+
+        heads = []
+        heads.append({
+            'bbox': [xmin, ymin, xmax, ymax],
+            'bbox_norm' : [xmin / float(width), ymin / float(height), xmax / float(width), ymax / float(height)],
+            'inout' : 1, #find out which one means in-frame and set it for all - there is not a label for this in GOO
+            'gazex' : dataset['gaze_cx'][i],
+            'gazey' : dataset['gaze_cy'][i],
+            'gazex_norm' : dataset['gaze_cx'][i],
+            'gazey_norm' : dataset['gaze_cy'][i],
+            'head_id' : 1, #need to change this to 1 - only one head per image in GOO.
+        
+        })
+        TEST_FRAMES.append({
+            'path' : location,
+            'heads' : heads,
+            'num_heads' : 1, #always one head per image in GOO
+            'width' : width,
+            'height' : height,
+        })
+
+    #create file to write edited dataset into
+    out_file = open(os.path.join(DATA_PATH, "test_preprocessed.json"), "w")
+    json.dump(TEST_FRAMES, out_file)
+
+    #TRAIN
+    #check if dataset downloaded, if not stream it from huggingface
+    if len(os.listdir(DATA_PATH)) == 0:
+        dataset = load_dataset("markytools/goosyntheticv3", split="train", streaming=True)  #issue with this - creates an iterable dataset not a dataset - doesnt act the same.
+        print("streaming dataset from huggingface, dataset: ", next(iter(dataset)) )
+    else:
+        dataset= load_dataset(DATA_PATH, split="train")
+        print("loading dataset from local path")
+
+    #need to differentiate between person bounding boxes and product bounding boxes - label of 25
+    TRAIN_FRAMES = []
+
+    for i in range(dataset.num_rows):
+
+        #get file location
+        img = dataset['image'][i]
+        filename = img.filename
+        location = os.path.join(DATA_PATH, filename)
+        width, height = img.size
+
+        #get head bbox
+        bboxes = re.findall(r"\[(.*?)\]", dataset['bboxes'][i][1:-1])
+        labels = dataset['labels'][i][1:-1].split(", ") #getting both bboxes and labels as lists
+
+        l_idx = labels.index('25') 
+        head_bbox = bboxes[l_idx]  
+        
+        bboxes = bboxes.remove(bboxes[l_idx]) 
+        labels = labels.remove('25')
+        
+        #xmin, xmax, ymin, ymax etc
+        xmin, ymin, xmax, ymax = head_bbox[1:-1].split(", ")
+        xmin = float(xmin)
+        ymin = float(ymin)
+        xmax = float(xmax)
+        ymax = float(ymax)
+
+        heads = []
+        heads.append({
+            'bbox': [xmin, ymin, xmax, ymax],
+            'bbox_norm' : [xmin / float(width), ymin / float(height), xmax / float(width), ymax / float(height)],
+            'inout' : 1, #find out which one means in-frame and set it for all - there is not a label for this in GOO
+            'gazex' : dataset['gaze_cx'][i],
+            'gazey' : dataset['gaze_cy'][i],
+            'gazex_norm' : dataset['gaze_cx'][i],
+            'gazey_norm' : dataset['gaze_cy'][i],
+            'head_id' : 1, #need to change this to 1 - only one head per image in GOO.
+        
+        })
+        TRAIN_FRAMES.append({
+            'path' : location,
+            'heads' : heads,
+            'num_heads' : 1, #always one head per image in GOO
+            'width' : width,
+            'height' : height,
+        })
+
+    #create file to write edited dataset into
+    out_file = open(os.path.join(DATA_PATH, "train_preprocessed.json"), "w")
+    json.dump(TRAIN_FRAMES, out_file)
 
 
 if __name__ == "__main__":
